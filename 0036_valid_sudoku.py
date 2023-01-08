e262c5d4e6df4f4df75ac38e9144ddde9ac2f4cd
Patrick Armengol <patrickarmengol@protonmail.com>
2023-01-08 03:44:31 +0000

    better 0036

--- 0036_valid_sudoku.py
+++ 0036_valid_sudoku.py
@@ -45,3 +45,30 @@ i forgot that i could invert a 2d array with zip(*board)
 also the square checking was a bit messy
 also i could have wrote a function for the testing of a group of 9
 """
+
+"""
+a way cleaner solution that uses dicts for rows, cols, squares and uses sets to check for duplicate nums
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)  # key = (r /3, c /3)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
+
+"""
