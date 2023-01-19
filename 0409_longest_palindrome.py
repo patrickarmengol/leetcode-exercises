7d471d5395700ad5c9b4d3d834ad7adc90f667ca
Patrick Armengol <patrickarmengol@protonmail.com>
2023-01-19 09:50:50 +0000

    0409

---
+++ 0409_longest_palindrome.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        c = Counter(s)
+        odds_m1 = [v-1 for v in c.values() if v % 2 != 0]
+        sum_odds = sum(odds_m1) + 1 if odds_m1 else 0
+        sum_evens = sum(v for v in c.values() if v % 2 == 0)
+        return sum_evens + sum_odds
+
+
+tests = [
+    "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
+]
+
+sol = Solution()
+
+for test in tests:
+    print(sol.longestPalindrome(test))
